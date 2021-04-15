10ccd17ab8885eb9bd730ddfc685d4e432664c8f
bankdore <bank_dore@hotmail.com>
2021-04-15 22:29:59 +0700

    Exercise4

---
+++ Exercise4_Tanes_T.py
@@ -0,0 +1,10 @@
+FE = 75.5
+GB = 83.7
+ICS = 84.3
+CP = 87.9
+
+print("---Your Score---")
+print(("Foundation English              :") , FE)
+print(("General Business                :") , GB)
+print(("Introduction to Computer Systems:") , ICS)
+print(("Computer Programming            :") , CP)
